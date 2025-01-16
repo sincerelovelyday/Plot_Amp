388cd0f3bfba0aef1861d6ef017279cd2d9c8175
sincerelovelyday <timeslover666@gmail.com>
2025-01-16 09:05:32 +0800

    UPlot_Amp.m

--- Plot_Amp.m
+++ Plot_Amp.m
@@ -8,7 +8,7 @@ Xk(2:end-1)=2*Xk(2:end-1);% 单边幅度谱
 f1=fs*(0:(N/2))/N;
 figure;
 subplot(2,1,1);plot(x);title('原始信号时域波形图');
-% w1=2*pi*f1/fs;
+% w1=2*pi*f1/fs;    % 信号频率分组
 subplot(2,1,2);plot(f1,Xk); title('原始语音信号采样后的频谱图');xlabel("归一化频率/\pi rad");
 
 end
